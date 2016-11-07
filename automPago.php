5bc3af3e014f78190f43919ff1817be192520f00
Enrique Rodriguez <quiqueblack7@hotmail.com>
2016-11-07 11:44:11 -0600

    test

---
+++ automPago.php
@@ -0,0 +1,36 @@
+<meta charset="UTF-8">
+<div class='t1'>Gracias por enviarnos tu pago</div>
+<div class='t2'>
+  Tu registro esta casi completado<br>
+  En breve recibiras tu usuario y contraseña para accesar a nuestra red
+</div>
+<div class='t3'>
+  Este mail se envió de manera automática, por favor no responder.
+  Si desea comunicarse con nosotros envie un correo a contacto@aimeds.org
+  o llame al telefono 5554731190 <br>
+  Asociación Internacional de Medicina y Salud A.C
+</div>
+
+<style>
+.t1{
+  margin-top: 20px;
+  font-family: "Century Gothic";
+  font-size: 24px;
+  width: 100%;
+  text-align: center;;
+}
+.t2{
+  margin-top: 20px;
+  font-family: "Century Gothic";
+  font-size: 16px;
+  width: 100%;
+  text-align: center;;
+}
+.t3{
+  margin-top: 40px;
+  font-family: "Century Gothic";
+  font-size: 12px;
+  width: 100%;
+  text-align: center;;
+}
+</style>
